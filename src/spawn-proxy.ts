7e561f85c675ed8bf328147422904c513d9ec25a
apatryda <a.apatryda@gmail.com>
2018-01-03 23:40:04 +0100

    [4] Spawn Proxy is now thenable

--- src/spawn-proxy.ts
+++ src/spawn-proxy.ts
@@ -1,23 +1,45 @@
 import { ChildProcess, spawn, SpawnOptions } from 'child_process';
 
-const _spawnProxy = function spawnProxyHandler(options?: SpawnOptions): ChildProcess {
+interface IPromiseThen<T> {
+  (resolve?: (T) => any, reject?: (any) => any): Promise<any>;
+}
+interface IThenable<T> {
+  then: IPromiseThen<T>;
+}
+
+const fSpawnProxy = function spawnProxyFunction(options?: SpawnOptions): ChildProcess & IThenable<ChildProcess> {
   const { path } = this;
   const args = [...path];
   const command = args.shift();
+  const spawnee: ChildProcess & {
+    then?: IPromiseThen<ChildProcess>;
+  } = spawn(command, args, options);
 
-  return spawn(command, args, options);
+  const spawnPromise: Promise<void> = new Promise((resolve, reject) => {
+    spawnee.once('exit', (code, signal) => {
+      if (code === 0) {
+        resolve();
+        return;
+      }
+
+      reject(Object.assign(new Error('Spawn failed.'), { code, signal }));
+    });
+  });
+
+  spawnee.then = spawnPromise.then.bind(spawnPromise);
+
+  return <ChildProcess & IThenable<ChildProcess>>spawnee;
 };
 
-const pathHandler = {
-  get({ path = [] } = {}, arg) {
-    const path_ = [...path, arg];
-    const spawnProxy_ = { path: path_ };
-    const spawnProxy = Object.assign(_spawnProxy.bind(spawnProxy_), spawnProxy_);
+const pathWalker = {
+  get({ path: _path = [] } = {}, arg) {
+    const path = [..._path, arg];
+    const oSpawnProxy = { path };
+    const spawnProxy = Object.assign(fSpawnProxy.bind(oSpawnProxy), oSpawnProxy);
 
-    return new Proxy(spawnProxy, pathHandler);
+    return new Proxy(spawnProxy, pathWalker);
   },
 };
 
-export const spawnProxy: any = new Proxy({},  pathHandler);
-
+export const spawnProxy: any = new Proxy({},  pathWalker);
 export default spawnProxy;
